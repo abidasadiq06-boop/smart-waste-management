d48073168032d1fd3f6f6ec0c241e3e6871b9515
abida <abidasadiq06@gmail.com>
2026-09-12 00:04:04 +0530

    added request form and citizen pickup tacking view

--- waste_app/urls.py
+++ waste_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('new-request/',views.create_request,name='new-request'),
-    
+    path('my-request/',views.request_list,name='request_list'),
+
 ]
